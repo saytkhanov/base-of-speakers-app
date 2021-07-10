a3b54aa2ffedcd387fa4d5d45363a4f3f2d64732
saytkhanov <saythanov2014@mail.ru>
2021-07-10 14:19:40 +0300

    category route

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = Router();
 router.use(require('./reviews.route'));
 router.use(require('./speakers.route'));
 router.use(require('./voices.route'));
+route.use(require('./categories.route'))
 
 module.exports = router;
