28c2d7741c71dd002b396a5656be70f1074c3732
saytkhanov <saythanov2014@mail.ru>
2021-07-11 12:08:19 +0300

    create controller for speaker and voice

---
+++ controllers/voice.controller.js
@@ -0,0 +1,41 @@
+const Voice = require('../models/Voice.model');
+
+module.exports.voicesControllers = {
+  getVoicesById: async (req, res) => {
+    try {
+      const getVoices = await Voice.find({speaker: req.params.id});
+      res.json(getVoices)
+    } catch (e) {
+      console.log(e.message)
+    }
+  },
+  createVoice: async (req, res) => {
+    const {audio} = req.body;
+    if (!audio) {
+      return res.status(401).json({
+        error: 'Необходимо выбрать файл',
+      });
+    }
+    try {
+      const createVoice = await new Voice({name});
+      await createVoice.save();
+      res.json(createVoice)
+    } catch (e) {
+      console.log(e.message)
+    }
+  },
+  deleteVoice: async (req, res) => {
+    try {
+      const deleteVoice = await Voice.findByIdAndDelete(req.params.id)
+      if (!deleteVoice) {
+        return res.json({
+          message: 'Не удалось удалить запись. Укажите верный ID',
+        });
+      }
+      res.json(deleteVoice);
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
+
+}
