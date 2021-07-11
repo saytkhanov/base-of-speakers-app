1ea0c27e2fc8ac9d7896f54ac38420ed7cda6f01
saytkhanov <saythanov2014@mail.ru>
2021-07-11 10:31:41 +0300

    create models Speaker and Voice

---
+++ models/Voice.model.js
@@ -0,0 +1,16 @@
+const {Schema, model, Types} = require('mongoose');
+
+const voiceSchema = new Schema({
+  name: String,
+  speaker: {
+    type: Types.ObjectId,
+    ref: "Speaker"
+  }
+},
+  {
+    timestamps: true
+  })
+
+const Voice = model("Voice", voiceSchema);
+
+module.exports = Voice
