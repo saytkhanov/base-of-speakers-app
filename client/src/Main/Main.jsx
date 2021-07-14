ef770ef112f7556d4ca829f38fb14495bbddc49a
Mohmad <magomedov951@mail.ru>
2021-07-14 11:26:46 +0300

    Categories and getAllSpeakers

--- client/src/Main/Main.jsx
+++ client/src/Main/Main.jsx
@@ -1,48 +1,55 @@
-import React, { useState } from 'react'
-import { Container} from '@material-ui/core'
-import { Switch,Route, Redirect } from 'react-router-dom'
-import AuthPage from '../pages/AuthPage';
-import LoginPage from '../pages/LoginPage'
-import { useDispatch, useSelector } from 'react-redux'
-import HomePage from '../pages/HomePage'
+import React, { useState } from "react";
+import { Container } from "@material-ui/core";
+import { Switch, Route, Redirect } from "react-router-dom";
+import AuthPage from "../pages/AuthPage";
+import LoginPage from "../pages/LoginPage";
+import { useDispatch, useSelector } from "react-redux";
+import HomePage from "../pages/HomePage";
+import SpeakersByCategory from "../components/categories/SpeakersByCategory";
+import AllSpeakers from "../components/allSpeakers/AllSpeakers";
 
-function Main (props) {
-
-  const token = useSelector(state => state.speakers.token);
+function Main(props) {
+  const token = useSelector((state) => state.speakers.token);
   // const tokenn = useSelector(state => state.speakers.token)
   // console.log(tokenn.id)
-if(!token) {
-  return (
-    <Container>
-      <Switch>
-        <Route path='/auth' exact >
-          <AuthPage/>
-        </Route>
-        <Route path='/login' >
-          <LoginPage/>
-        </Route>
-        <Redirect to='/'/>
-      </Switch>
-    </Container>
-  )
-}
+  if (!token) {
+    return (
+      <Container>
+        <Switch>
+          <Route path="/auth" exact>
+            <AuthPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Container>
+    );
+  }
 
   return (
     <Container>
       <Switch>
-        <Route path='/' exact>
-          <HomePage/>
+        <Route path="/" exact>
+          <HomePage />
         </Route>
-        <Route path='/auth' >
-          <AuthPage/>
+        <Route path="/auth">
+          <AuthPage />
         </Route>
-        <Route path='/login' >
-          <LoginPage/>
+        <Route path="/login">
+          <LoginPage />
         </Route>
-        <Redirect to='/speaker'/>
+        <Route path="/category/:id/speakers">
+          <SpeakersByCategory />
+        </Route>
+        <Route>
+          <AllSpeakers/>
+        </Route>
+        <Redirect to="/speaker" />
       </Switch>
     </Container>
-  )
+  );
 }
 
-export default Main
+export default Main;
---
+++ client/src/components/categories/SpeakersByCategory.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { Box } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { getSpeakers } from "../../redux/features/speakers";
+import { useParams } from "react-router-dom";
+
+function SpeakersByCategory(props) {
+  const dispatch = useDispatch();
+  const speakers = useSelector((state) => state.speakers.items);
+
+  const categoryId = useParams();
+
+  console.log(categoryId);
+
+  useEffect(() => dispatch(getSpeakers()), [dispatch]);
+
+  return (
+    <Box>
+      {speakers.map((item) => {
+        if (item.category === categoryId.id) {
+          return <Box>{item.firstName}</Box>;
+        }
+      })}
+    </Box>
+  );
+}
+
+export default SpeakersByCategory;
